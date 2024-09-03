8771f1d86dff04b728a0a52f1966fc6635013ed6
gorsol89 <gorsunsol@gmail.com>
2024-09-03 14:41:11 +0200

    Made the api call - it works. Had trouble with CORS, now fixed.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const url = "https://api.noroff.dev/api/v1/rainy-days";
+const baseUrl = "http://rainydays.local/wp-json/wc/store/products";
+const productContainer = document.querySelector('.products');
 
 async function getProducts() {
     const resultsContainer = document.querySelector("#container");
     resultsContainer.innerHTML = '<div class="spinner"></div>'; // Display spinner before loading products
 
     try {
-        const response = await fetch(url);
+        const response = await fetch(baseUrl);
         const products = await response.json();
 
         resultsContainer.innerHTML = "";
@@ -13,9 +14,9 @@ async function getProducts() {
         products.forEach(function (product) {
             resultsContainer.innerHTML += `
             <div class="card">
-                <img src="${product.image}" alt="${product.title}">
-                <h3>${product.title}</h3>
-                <p>Price: ${product.price}</p>
+                <img src="${product.images[0].src}" alt="${product.name}">
+                <h3>${product.name}</h3>
+                <p>Price: ${product.prices.price} ${product.prices.currency_code}</p>
                 <a href="Jacket-detail.html?id=${product.id}" class="view-more">View More</a>
             </div>`;
         });
@@ -25,13 +26,4 @@ async function getProducts() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    getProducts(); // Call getProducts function when the DOM is fully loaded
-
-    document.getElementById('contactForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-
-        // Perform any form data validation or processing here (optional)
-
-    });
-});
+getProducts();
